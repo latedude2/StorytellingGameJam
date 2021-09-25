65dbb483cc07bc412c24cd0237f2b6b941995c6e
latedude2 <simasc10@gmail.com>
2021-09-25 21:07:33 +0200

    Make enemy chase rover

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,18 +5,21 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     Rover rover;
+    float chaseDistance = 0.3f;
     private float speed = 0.05f;
     private float mapSize;
 
     void Start() {
+        rover = transform.parent.Find("Rover").GetComponent<Rover>();
         mapSize = 0.5f * transform.parent.localScale.x;
         //rover = transform.parent.GetComponentInChildren<Rover>();
         Vector2 thrust = new Vector2(Random.Range(-speed, speed), (Random.Range(-speed, speed)));
-        GetComponent<Rigidbody2D>().velocity = thrust; 
+        GetComponent<Rigidbody2D>().velocity = thrust.normalized * speed; 
     }
 
     void Update() {
         wrapAroundMap();
+        ChaseRover();
     }
     void wrapAroundMap(){
 
@@ -38,4 +41,15 @@ public class Enemy : MonoBehaviour
         }
     }
 
+    void ChaseRover()
+    {
+        //Debug.Log(Vector2.Distance(new Vector2(transform.position.x, transform.position.y), new Vector2(rover.transform.position.x, rover.transform.position.y)));
+        Vector2 roverPosition = new Vector2(rover.transform.position.x, rover.transform.position.y);
+        Vector2 enemyPosition = new Vector2(transform.position.x, transform.position.y);
+        if(Vector2.Distance(enemyPosition, roverPosition) < chaseDistance)
+        {
+            Vector2 thrust = roverPosition - enemyPosition;
+            GetComponent<Rigidbody2D>().velocity = thrust.normalized * speed;
+        }
+    }
 }
